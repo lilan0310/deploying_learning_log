71c2a64b64c721946c5efaaaa4581dfe27dd8086
liyanlan <3344358203@qq.com>
2024-10-27 18:13:09 +0800

    Initial commit

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL='/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS','localhost').split(',')
 
 
 # Quick-start development settings - unsuitable for production
