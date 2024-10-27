14fb8cd57f98682e0b2fb264d068a00513c01fea
liyanlan <3344358203@qq.com>
2024-10-27 21:03:24 +0800

    Initial commit

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-mibxt7sboxg%r)9t6z=a6@weyo(thgbknfcwgyowgx9$5s4&=!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS','localhost').split(',')
+ALLOWED_HOSTS = ['liyanlan.fra1.zeabur.app','localhost','127.0.0.1']
 
 # Application definition
 
